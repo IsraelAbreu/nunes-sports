d3f55fac2c3fe5441481c3c46a72ea2d94ef3702
IsraelAbreu <israeloliveira18@hotmail.com>
2024-08-24 21:27:03 -0300

    refactor: ajuste do layout da listagem de produtos

--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -1,38 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- Importanto Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('/css/ListarProdutos/listarProdutos.css')}}">
 </head>
+
 <body>
     <div class="nav-principal">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">Nunes Sports</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('produtos.listar') }}">Início</a>
-                  </li>
-                </ul>
-                <div class="d-flex" role="search">
-                    <a class="btn btn-success right" href="{{route('produtos.novo')}}">Cadastrar Produto</a>
+                <a class="navbar-brand" href="#">Nunes Sports</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page"
+                                href="{{ route('produtos.listar') }}">Início</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex" role="search">
+                        <a class="btn btn-success right" href="{{ route('produtos.novo') }}">Cadastrar Produto</a>
+                    </div>
                 </div>
-              </div>
             </div>
-          </nav>
+        </nav>
     </div>
     {{-- Conteúdo Principal --}}
     <div class="container">
         @yield('content')
     </div>
-<!-- Importando Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- Importando Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
--- resources/views/main/ListaProdutos.blade.php
+++ resources/views/main/ListaProdutos.blade.php
@@ -1,44 +1,46 @@
 @extends('Layouts.main')
 @section('content')
-@if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
     <div class="container">
         <h1 class="text-center m-5">Meus Produtos</h1>
         <div class="tabela_produtos">
             <table class="table">
                 <thead>
-                  <tr>
-                    <th scope="col">Código Produto</th>
-                    <th scope="col">Produto</th>
-                    <th scope="col">Descrição</th>
-                    <th scope="col">Preço</th>
-                    <th class="text-center" scope="col">Ações</th>
-                  </tr>
+                    <tr>
+                        <th scope="col">Código Produto</th>
+                        <th scope="col">Produto</th>
+                        <th scope="col">Descrição</th>
+                        <th scope="col">Preço</th>
+                        <th class="text-center" scope="col">Ações</th>
+                    </tr>
                 </thead>
                 <tbody>
-                  @foreach ($produtos as $produto)
-                  <tr>
-                    <th scope="row">{{$produto->codigo_produto}}</th>
-                    <td>{{$produto->nome_produto}}</td>
-                    <td>{{$produto->descricao_produto}}</td>
-                    <td>{{$produto->preco_produto}},00</td>
-                    <td>
-                        <div class="text-center">
-                            <a class="btn btn-primary" href="{{route('produtos.ver', $produto->id)}}">Ver</a>
-                            <form action="{{route('produtos.deletar', $produto->id)}}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Excluir</button>
-                            </form>
-                        </div>
-                    </td>
-                  </tr>
-                  @endforeach
+                    @foreach ($produtos as $produto)
+                        <tr>
+                            <td>{{ $produto->codigo_produto }}</td>
+                            <td>{{ $produto->nome_produto }}</td>
+                            <td>{{ $produto->descricao_produto }}</td>
+                            <td>{{ $produto->preco_produto }},00</td>
+                            <td class="text-center">
+                                <div class="mb-1">
+                                    <a class="btn btn-primary btn-editar-produto" href="{{ route('produtos.ver', $produto->id) }}">Ver</a>
+                                </div>
+                                <div class="">
+                                    <form action="{{ route('produtos.deletar', $produto->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-excluir-produto">Excluir</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
-              </table>
+            </table>
         </div>
     </div>
 @endsection
